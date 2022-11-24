0491a9e1c5e4c7d0fea64f00039ea5e0daaf13fd
xyber <gvanderbiezen@gmail.com>
2022-11-24 01:09:27 +0100

    test

--- leden_adm/resources/views/ledendash.blade.php
+++ leden_adm/resources/views/ledendash.blade.php
@@ -3,7 +3,7 @@
     <div class="py-0">
         <div class="max-w-4xl mx-auto p-2 bg-white overflow-hidden border-t-[1px] border-gray-200 shadow-sm">
                 <div>
-                    Welcome! You're logged in!
+                    Welcome! You're logged in! Yes working!
 		<br><br>
 		<div class="searchbox"> Searh Box Input Form</div>
 		<br>
